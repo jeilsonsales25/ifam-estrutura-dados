51f721569e8c474fb49cdd4123e7c39dab0a74b8
jeilsonsales.25 <jeilsonsales.2502@gmail.com>
2026-03-20 19:49:47 -0400

    Add files via upload

---
+++ espalhamento_minimo.js
@@ -0,0 +1,9 @@
+/*const numeros = [9,6,1,4,2,3]
+...numeros
+    menor= Math.min(numeros[i])
+    alert(menor)
+}*/
+const numeros = [9, 6, 1, 4, 2, 3];
+menor = Math.min(...numeros);
+alert(menor);
+
---
+++ troca.js
@@ -0,0 +1,6 @@
+let numeros1 = 5, numero2 =2, temp
+alert(`${numeros1}- ${numero2}`)
+temp = numeros1
+numeros1 = numero2
+numero2 = temp
+alert(`Valor 1= ${numeros1}\nValor 2= ${numero2}`)
